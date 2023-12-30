74ac5229335419c31740b601e3dcf38b0efdb62e
Maimuna <maimunaibrahim808@gmail.com>
2023-12-30 12:54:21 -0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -33,20 +33,43 @@
 // }
 
 import React, { useState } from "react";
-import Hello from "./components/Hello";
+import Note from "./components/Note";
 
 const App = (props) => {
-  console.log("Hello from component");
-  const { notes } = props;
+  console.log("Note from component");
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const notObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(notObject));
+    setNewNote("");
+    console.log("button clicked", event.target);
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].contenet}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
