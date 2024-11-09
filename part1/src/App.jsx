4a14fabd5e261468bd6b0aa8e5c2834d324f5f61
Maimuna <maimunaibrahim808@gmail.com>
2024-11-09 03:04:00 +0100

    new action

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,6 +4,7 @@ import Note from './components/Note'
 
 import "./App.css";
 import axios from 'axios'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -14,13 +15,78 @@ const App = () => {
     console.log('effect')
     axios
       .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
   console.log('render', notes.length, 'notes')
 
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+  const note = notes.find(n => n.id === id)
+  const changedNote = { ...note, important: !note.important }
+
+
+  noteService
+  .update(id, changedNote)
+  .then(returnedNote => {
+    setNotes(notes.map(note => note.id === id ? returnedNote : note))
+  })
+  .catch(error => {
+    alert(
+      `the note '${note.content}' was already deleted from server`
+    )
+    setNotes(notes.filter(n => n.id !== id))
+  })
+}
+
+const addNote = (event) => {
+event.preventDefault()
+const noteObject = {
+  content: newNote,
+  important: Math.random() > 0.5
+}
+
+noteService
+  .create(noteObject)
+  .then(returnedNote => {
+    setNotes(notes.concat(returnedNote))
+    setNewNote('')
+  })
+
+
+
+
+
+
+  axios.put(url, changedNote).then(response => {
+    setNotes(notes.map(n => n.id === id ? response.data : n))
+  })
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>      
+      <ul>
+        {notesToShow.map(note => 
+          <Note
+            key={note.id}
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </ul>
+      // ...
+    </div>
+  )
+
+}
 export default App;
 
 
